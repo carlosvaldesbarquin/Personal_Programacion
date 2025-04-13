aad7ac2500dec30d06761033a0814c51bd2bd4b8
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2025-04-13 21:14:38 +0200

    Codigo Finalizado

--- valdesCarlos/Sistema.java
+++ valdesCarlos/Sistema.java
@@ -4,8 +4,8 @@ public class Sistema {
     public static void main(String[] args) {
         Cafeteria cafeteria = new Cafeteria(5);  // máximo 5 productos en el menú
 
-        cafeteria.agregarProductoAlMenu(new Producto("Café", 2.5, true));
-        cafeteria.agregarProductoAlMenu(new Producto("Té", 2.0, true));
+        cafeteria.agregarProductoAlMenu(new Producto("Café", 1.5, true));
+        cafeteria.agregarProductoAlMenu(new Producto("Té", 1.40, true));
         cafeteria.agregarProductoAlMenu(new Producto("Muffin", 3.0, true));
         cafeteria.agregarProductoAlMenu(new Producto("Sandwich", 4.0, true));
 
