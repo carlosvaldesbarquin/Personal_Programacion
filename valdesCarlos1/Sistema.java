232c3978be72d534f8f255d168bde0e1c9edca81
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2025-04-14 19:49:41 +0200

    Creacion de la clase Sistema

---
+++ valdesCarlos1/Sistema.java
@@ -0,0 +1,8 @@
+package valdesCarlos1;
+
+public class Sistema {
+    public static void main(String[] args) {
+        Alquiler alquiler = new Alquiler();
+        alquiler.iniciarAlquiler();
+    }
+}
